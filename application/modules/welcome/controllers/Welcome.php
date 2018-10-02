b9cba0ace31967ec17f8dc4a30d92528cc4e4e36
alzen8work <alzen8work@gmail.com>
2018-10-02 17:58:20 +0800

    #2 custom validtion in any modules

--- application/modules/welcome/controllers/Welcome.php
+++ application/modules/welcome/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends MX_Controller {
+class Welcome extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
